c713ef4ea410b9566461afd328f992f4f58745dd
Simeon Olorunninwo <simeontoluwanimi@gmail.com>
2019-11-06 04:11:16 +0100

    new work

---
+++ Chapter02/exercises/src/exercise2_18/Shapes.java
@@ -0,0 +1,20 @@
+package exercise2_18;
+// Program displays a box, an oval, an arrow and a diamond using asterisks
+
+public class Shapes 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("**********     ***       *          *");
+		System.out.println("*        *    *   *     ***        * *");
+		System.out.println("*        *   *	   *   *****      *   *");
+		System.out.println("*        *   *     *  	 *       *     *");
+		System.out.println("*        *   *     *     *      *       *");
+		System.out.println("*        *   *     *     *       *     *");
+		System.out.println("*        *   *     *     *        *   *");
+		System.out.println("*        *    *   *      *         * *");
+		System.out.println("**********     ***       *          *");
+		
+	}
+
+}
